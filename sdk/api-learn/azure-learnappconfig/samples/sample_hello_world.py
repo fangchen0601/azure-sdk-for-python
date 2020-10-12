508047d71ba8af56fca45a610aa288ee19caabc1
Fang Chen <16232002+fangchen0601@users.noreply.github.com>
2020-10-12 13:48:06 -0700

    add layer, new model

--- sdk/api-learn/azure-learnappconfig/samples/sample_hello_world.py
+++ sdk/api-learn/azure-learnappconfig/samples/sample_hello_world.py
@@ -6,10 +6,30 @@
 # license information.
 # --------------------------------------------------------------------------
 
-def main():
-    # Add your sample here
-    pass
+import os
+from colorama import init, Style, Fore
+init()  # Initialize colorama for colored console output
 
+from azure.identity import DefaultAzureCredential
+from azure.learnappconfig import AppConfigurationClient
+from azure.core.exceptions import ResourceNotFoundError
+
+def main():
+    endpoint = os.environ['API-LEARN_ENDPOINT']
+    credentials = DefaultAzureCredential()
+    client = AppConfigurationClient(endpoint, credentials)
+
+    try:
+        color_setting = client.get_configuration_setting(os.environ['API-LEARN_SETTING_COLOR_KEY'])
+        color = color_setting.value.upper()
+        text_setting = client.get_configuration_setting(os.environ['API-LEARN_SETTING_TEXT_KEY'])
+        greeting = text_setting.value
+    except ResourceNotFoundError:
+        color = 'RED'
+        greeting = 'Default greeting'
+
+    color = getattr(Fore, color)
+    print(f'{color}{greeting}{Style.RESET_ALL}')
 
 if __name__ == "__main__":
     main()
