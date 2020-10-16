2496764e9768d937a692e75115465577e4aaf9a1
Fang Chen <16232002+fangchen0601@users.noreply.github.com>
2020-10-16 12:08:45 -0700

    init member fields explicitly in async test class

--- sdk/api-learn/azure-learnappconfig/tests/test_app_config_async.py
+++ sdk/api-learn/azure-learnappconfig/tests/test_app_config_async.py
@@ -12,14 +12,18 @@ class AppConfigurationClientTest(AsyncAppConfigTestCase):
     def setUp(self):
         super(AppConfigurationClientTest, self).setUp()
         self.app_config_url = self.set_value_to_scrub('APP_CONFIG_URL', APP_CONFIG_URL)
+        self.env_color = os.environ.get('API-LEARN_SETTING_COLOR_VALUE', "Green")
+        self.env_color_key = os.environ.get('API-LEARN_SETTING_COLOR_KEY', "FontColor")
+        self.env_greeting = os.environ.get('API-LEARN_SETTING_TEXT_VALUE', "Hello World!")
+        self.env_greeting_key = os.environ.get('API-LEARN_SETTING_TEXT_KEY', "Greeting")
 
     @AsyncAppConfigTestCase.await_prepared_test
     async def test_get_key_value(self):
         client = self.create_basic_client(AppConfigurationClient, account_url=self.app_config_url)
         assert client is not None
 
-        assert self.env_color == await client.get_configuration_setting(self.env_color_key)
-        assert self.env_greeting == await client.get_configuration_setting(self.env_greeting_key)
+        assert self.env_color == await client.get_configuration_setting(self.env_color_key)['value']
+        assert self.env_greeting == await client.get_configuration_setting(self.env_greeting_key)['value']
 
     @AsyncAppConfigTestCase.await_prepared_test
     async def test_get_invalid_key(self):
